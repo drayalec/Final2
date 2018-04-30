b5628841c95f1b0f63089c69bf817045360f68ec
Alec DiRaimondo <adiraimondo562@students.mchenry.edu>
2018-04-29 19:37:30 -0500

    week2

--- app/src/main/java/com/example/draya/finalpractice/MainActivity.java
+++ app/src/main/java/com/example/draya/finalpractice/MainActivity.java
@@ -1,32 +1,28 @@
 package com.example.draya.finalpractice;
 
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.draya.finalpractice.MainClassList;
-import com.example.draya.finalpractice.R;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    public enum LoginSuccess{
+    public enum LoginSuccess {
         login(1),
         password(2),
         success(0);
 
         private int intValue;
 
-        LoginSuccess(int value){
+        LoginSuccess(int value) {
             intValue = value;
         }
-        public int getIntValue(){
+
+        public int getIntValue() {
             return intValue;
         }
     }
@@ -38,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         final TextView txtLogin = (TextView) findViewById(R.id.idLoginText);
         final TextView txtPassword = (TextView) findViewById(R.id.idPasswordText);
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
@@ -47,15 +42,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast toastError = null;
-                LoginSuccess isOK  = null;
+                LoginSuccess isOK = null;
 
-                switch(CheckLogin(txtLogin.getText().toString(),txtPassword.getText().toString())){
+                switch (CheckLogin(txtLogin.getText().toString(), txtPassword.getText().toString())) {
                     case login:
-                        toastError.makeText(getApplicationContext(),getString(R.string.errMessageLogin), toastError.LENGTH_SHORT).show();
+                        toastError.makeText(getApplicationContext(), getString(R.string.errMessageLogin), toastError.LENGTH_SHORT).show();
                         txtLogin.requestFocus();
                         break;
                     case password:
-                        toastError.makeText(getApplicationContext(),getString(R.string.errMessagePassword), toastError.LENGTH_SHORT).show();
+                        toastError.makeText(getApplicationContext(), getString(R.string.errMessagePassword), toastError.LENGTH_SHORT).show();
                         txtPassword.requestFocus();
                         break;
                     default:
@@ -64,25 +59,23 @@ public class MainActivity extends AppCompatActivity {
                 }
 
 
-
             }
 
         });
 
 
-
     }
 
 
-    LoginSuccess CheckLogin(String txtLogin, String txtPassword){
+    LoginSuccess CheckLogin(String txtLogin, String txtPassword) {
         String holdLogin = "Alec";
         String holdPass = "password";
 
-        if(!(holdLogin.equals(txtLogin))){
+        if (!(holdLogin.equals(txtLogin))) {
             return LoginSuccess.login;
         }
 
-        if (!(holdPass.equals(txtPassword))){
+        if (!(holdPass.equals(txtPassword))) {
             return LoginSuccess.password;
         }
 
--- app/src/main/java/com/example/draya/finalpractice/MainClassList.java
+++ app/src/main/java/com/example/draya/finalpractice/MainClassList.java
@@ -1,8 +1,8 @@
 package com.example.draya.finalpractice;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -37,7 +37,7 @@ public class MainClassList extends AppCompatActivity {
         swDegreeCert.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     spnDegree.setVisibility(View.VISIBLE);
                     txtDegree.setVisibility(View.VISIBLE);
                     spnCertificate.setVisibility(View.GONE);
@@ -54,7 +54,7 @@ public class MainClassList extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkData()){
+                if (checkData()) {
                     String doBirth = "";
                     doBirth = spMonth.getSelectedItem().toString() + "/" + txtDay.getText().toString() + "/" + txtYear.getText().toString();
 
@@ -62,18 +62,18 @@ public class MainClassList extends AppCompatActivity {
                     // startActivity(new Intent(MainClassList.this, ChooseClass.class));
 
                     //new way
-                    Intent nextScreen = new Intent(MainClassList.this, Chooseclass.class);
+                    Intent nextScreen = new Intent(MainClassList.this, ChooseClass.class);
                     nextScreen.putExtra("FirstName", firstName.getText().toString());
                     nextScreen.putExtra("LastName", lastName.getText().toString());
                     nextScreen.putExtra("Phone", phone.getText().toString());
                     nextScreen.putExtra("BirthDate", doBirth);
 
-                    if (spnDegree.getVisibility() == View.VISIBLE){
-                        nextScreen.putExtra("isDegreeCert","Degree");
+                    if (spnDegree.getVisibility() == View.VISIBLE) {
+                        nextScreen.putExtra("isDegreeCert", "Degree");
                         nextScreen.putExtra("degreeCert", spnDegree.getSelectedItem().toString());
 
                     } else {
-                        nextScreen.putExtra("isDegreeCert","Certificate");
+                        nextScreen.putExtra("isDegreeCert", "Certificate");
                         nextScreen.putExtra("degreeCert", spnCertificate.getSelectedItem().toString());
                     }
                     //Start Activity
@@ -84,8 +84,7 @@ public class MainClassList extends AppCompatActivity {
     }
 
 
-
-    private boolean checkData(){
+    private boolean checkData() {
         final EditText firstName = (EditText) findViewById(R.id.txtFirstName);
         final EditText lastName = (EditText) findViewById(R.id.txtLastName);
         final EditText phone = (EditText) findViewById(R.id.txtPhone);
@@ -93,31 +92,31 @@ public class MainClassList extends AppCompatActivity {
         final EditText txtYear = (EditText) findViewById(R.id.txtYear);
 
 
-        if(firstName.getText().toString().isEmpty()){
+        if (firstName.getText().toString().isEmpty()) {
             //error
             firstName.setError("Invalid First Name");
             firstName.requestFocus();
             return false;
         }
 
-        if(lastName.getText().toString().isEmpty()){
+        if (lastName.getText().toString().isEmpty()) {
             lastName.setError("Invalid Last Name");
             lastName.requestFocus();
             return false;
         }
 
-        if(phone.getText().toString().isEmpty()){
+        if (phone.getText().toString().isEmpty()) {
             phone.setError("Invalid Phone Number");
             phone.requestFocus();
             return false;
         }
 
-        if(txtDay.getText().toString().isEmpty()){
+        if (txtDay.getText().toString().isEmpty()) {
             txtDay.setError("Invalid Date Selection");
             txtDay.requestFocus();
             return false;
         }
-        if(txtYear.getText().toString().isEmpty()){
+        if (txtYear.getText().toString().isEmpty()) {
             txtYear.setError("Invalid Date Selection");
             txtYear.requestFocus();
             return false;
